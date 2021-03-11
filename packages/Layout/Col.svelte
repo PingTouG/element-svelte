9502268e8085abd85809074cefa6b2c06ff8e0c3
PingTouG <2798874773@qq.com>
2021-03-11 18:22:36 +0800

    chore(packages:layout): 添加Row和Col组件

---
+++ packages/Layout/Col.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { getContext } from 'svelte'
+	import { GUTTER } from './store'
+	import { preffixConcat } from '../utils/tools'
+	export let span = 24
+	export let offset = 0
+	export let push = 0
+	export let pull = 0
+
+	const gutter: number = getContext(GUTTER)
+
+	$: style = gutter
+		? `padding-left: ${gutter / 2}px;padding-right: ${gutter / 2}px`
+		: undefined
+	$: spanClass = preffixConcat(span !== 24, 'es-col-span-', span)
+	$: offsetClass = preffixConcat(offset, 'es-col-offset-')
+	$: pushClass = preffixConcat(push, 'es-col-push-')
+	$: pullClass = preffixConcat(pull, 'es-col-pull-')
+	$: classAttr = `es-col ${spanClass} ${offsetClass} ${pushClass} ${pullClass}`.trim()
+</script>
+
+<div class="{classAttr}" style="{style}">
+	<slot />
+</div>
+
+<style src="./_col.scss" global></style>
---
+++ packages/Layout/Row.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { setContext } from 'svelte'
+	import { GUTTER } from './store'
+	import { preffixConcat, trimConcat } from '../utils/tools'
+
+	let className = ''
+	export { className as class }
+	export let gutter = 0
+	export let type: string
+	export let justify = 'start'
+	export let align = 'top'
+
+	const isFlex = type === 'flex'
+	const classAttr = trimConcat(
+		'es-row',
+		className,
+		preffixConcat(isFlex, '', type),
+		preffixConcat(isFlex, 'justify-', justify),
+		preffixConcat(isFlex, 'align-', align),
+	)
+
+	setContext(GUTTER, gutter)
+</script>
+
+<div class="{classAttr}">
+	<slot />
+</div>
+
+<style src="./_row.scss" global></style>
