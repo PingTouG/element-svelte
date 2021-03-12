8e9ff12880e499f0a6c0dd28c8d3e537d0354c79
PingTouG <2798874773@qq.com>
2021-03-12 14:14:58 +0800

    feat(package): 添加Link组件

---
+++ packages/Link/index.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import { preffixConcat, trimConcat } from '../utils/tools'
+	import { createEventDispatcher } from 'svelte'
+	import Icon from '../Icon/index.svelte'
+
+	let className = ''
+	export { className as class }
+
+	export let disabled = false
+	export let href: string
+	export let icon: string
+	export let type: string
+	export let underline = true
+	export let target = '_self'
+
+	$: classAttr = trimConcat(
+		'es-link',
+		className,
+		preffixConcat(disabled, '', 'disabled'),
+		preffixConcat(type, 'link-type-'),
+		preffixConcat(underline, '', 'underline'),
+	)
+
+	const dispatch = createEventDispatcher()
+	function onClick() {
+		if (!disabled || !href) {
+			dispatch('click')
+		}
+	}
+</script>
+
+<a
+	class="{classAttr}"
+	href="{disabled ? null : href}"
+	on:click="{onClick}"
+	target="{target}"
+>
+	{#if icon}
+		<Icon name="{icon}" />
+	{/if}
+	{#if $$slots.default}
+		<span class="es-link--inner">
+			<slot />
+		</span>
+	{/if}
+	{#if $$slots.icon}
+		<slot name="icon" />
+	{/if}
+</a>
+
+<style src="./_index.scss" global></style>
