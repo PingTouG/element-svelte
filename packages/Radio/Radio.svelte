2068df788722cf386552779af6eb5d3f0c430dfb
PingTouG <2798874773@qq.com>
2021-03-17 19:59:33 +0800

    feat(package): Radio

---
+++ packages/Radio/Radio.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { trimConcat, preffixConcat } from '../utils/tools'
+	import { createEventDispatcher } from 'svelte'
+
+	let className = ''
+	export { className as class }
+	export let value: string | number | boolean
+	export let label: string | number | boolean
+	export let disabled = false
+	export let border = false
+	export let size: string
+	export let name: string
+
+	$: checked = value === label
+	$: classAttr = trimConcat(
+		'es-radio',
+		className,
+		preffixConcat(border && size, '', size),
+	)
+
+	const dispatch = createEventDispatcher()
+	const onClick = () => {
+		if (!disabled) {
+			value = label
+			dispatch('click', label)
+		}
+	}
+</script>
+
+<label
+	class="{classAttr}"
+	class:disabled
+	class:border
+	class:checked
+	role="radio"
+	on:click="{onClick}"
+>
+	<input class="es-radio__input" type="radio" name="{name}" hidden />
+	<div class="es-radio__container">
+		<span class="es-radio__label" class:checked>
+			<slot />
+			{#if !$$slots.default}
+				{label}
+			{/if}
+		</span>
+	</div>
+</label>
+
+<style src="./_radio.scss" global></style>
